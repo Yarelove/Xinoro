6cbab437b537cff5f15d3c2145e739a70378007a
Egor Udovin <57674283+xoheveras@users.noreply.github.com>
2022-02-07 14:03:34 +0300

    update config

--- core/views/admin/panel.php
+++ core/views/admin/panel.php
@@ -8,8 +8,8 @@
     <link rel="stylesheet" href="vendor/css/panel.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 	<!-- Bootstrap 5 -->
-    <link rel="stylesheet" href="vendor/assets/css/bootstrap.css">
-    <script src="vendor/assets/js/bootstrap.js"></script>
+    <!-- <link rel="stylesheet" href="vendor/assets/css/bootstrap.css">
+    <script src="vendor/assets/js/bootstrap.js"></script> -->
     <title>Админ Панель</title>
 </head>
 <body>
