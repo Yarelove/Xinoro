21f58f647d3e9aa30370e9b0d71ec7d4aee4db7d
Egor Udovin <57674283+xoheveras@users.noreply.github.com>
2022-02-10 02:34:48 +0300

    add xohebb

--- core/views/admin/editor.php
+++ core/views/admin/editor.php
@@ -1,56 +1,140 @@
-<html>
-	<head>
-		<link rel="stylesheet" type="text/css" href="vendor/css/panel.css">
-	</head>
-	<body>
-		<header>
-			<a href="admin/panel" class="logo"><img src="https://github.com/xoheveras/Xinoro/blob/main/default_config/default_img/logob.png?raw=true" alt=""></a>
-			<div class="nav">
-				<ul>
-					<li><a href="admin/panel"><img src="https://github.com/xoheveras/Xinoro/blob/main/default_config/default_img/Main-Icon.png?raw=true" alt=""></a></li>
-					<li><a href="admin/content"><img src="https://github.com/xoheveras/Xinoro/blob/main/default_config/default_img/Main-Page-Icon.png?raw=true" alt=""></a></li>
-					<li><a href="admin/database"><img src="https://github.com/xoheveras/Xinoro/blob/main/default_config/default_img/Main-User-Icon.png?raw=true" alt=""></a></li>
-					<li><a href="admin/setting"><img src="https://github.com/xoheveras/Xinoro/blob/main/default_config/default_img/Main-Setting-Icon.png?raw=true" alt=""></a></li>
-				</ul>
-			</div>
-		</header>
-		<content>
-			<iframe src="http://<?php echo $_SERVER['HTTP_HOST']; ?><?php echo json_decode(file_get_contents("config/setting.json"),true)['base_url']; ?><?php echo $_GET['c']."/".$_GET['a'] ?>" width="110%" height="895px" align="left">
-			    Ваш браузер не поддерживает плавающие фреймы!
-			</iframe>
-			<div class="editor-box">
-				<div id="tabsEditor" class="tabs">
-					<div id="t1" class="tabs-box">Php</div>
-					<div id="t2" class="tabs-box">Css</div>
-					<div id="t3" class="tabs-box">Js</div>
-					<div id="t4" class="tabs-box">Store</div>
-				</div>
-				<form method='POST' action=''>
-					<!-- t1 -->
- 					<textarea name="file"><?php echo file_get_contents("core/views/".$_GET['c']."/".$_GET['a'].".php"); ?></textarea>
-					<input type='submit' class="btn-box" name='savepages' value='Обновить'>
-					<!-- t2 -->
-<!-- 					<?php 
-						if(is_file("core/views/".$_GET['c']."/".$_GET['a']."_style.json")) 
-						{
-							$include_file = json_decode(file_get_contents("core/views/".$_GET['c']."/".$_GET['a']."_style.json"),true);
-							foreach($include_file["css"] as $value)
-							{
-							echo '
-								<div class="style-box">
-									<div class="style-label">'.$value.'</div>
-									<div class="style-edit-btn">Edit</div>
-									<div class="style-delete-btn">Удалить</div>
-								</div>
-							';
-							}
-						}
-					?>
-					<div class="style-box style-label create-style-btn">Создать</div> -->
-				</form>
-			</div>
-		</content>
-	</body>
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="vendor/css/xinoro_admin_editor.css">
+  </head>
+  <body>
+    <ul class="nav p-2 nav-pills justify-content-center">
+      <li class="nav-item">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Три</a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <div class="carousel slide" data-ride="carousel" id="blogCarousel">
+            <div class="container carousel-inner">
+              <div class="carousel-item active">
+                <div class="row">
+                  <div class="col-md-2">
+                    <a href="#">
+                      <img class="anime" src="https://i.pinimg.com/originals/d6/0e/a3/d60ea3683ce4e8d763e46a829f47f5cd.jpg" alt="">
+                    </a>
+                  </div>
+                  <div class="col-md-2">
+                   <a href="#">
+                     <img class="anime" src="https://proprikol.ru/wp-content/uploads/2020/05/kartinki-glaza-anime-42.jpg" alt="">
+                   </a>
+                 </div>
+                  <div class="col-md-2">
+                   <a href="#">
+                     <img class="anime" src="https://get.wallhere.com/photo/2000x1415-px-anime-girls-schoolgirls-1501001.jpg" alt="">
+                   </a>
+                 </div>
+                  <div class="col-md-2">
+                   <a href="#">
+                     <img class="anime" src="https://phonoteka.org/uploads/posts/2021-05/1621950877_53-phonoteka_org-p-krasivie-anime-arti-krasivo-57.jpg" alt="">
+                   </a>
+                 </div>
+                 <div class="col-md-2">
+                   <a href="#">
+                     <img class="anime" src="https://w-dog.ru/wallpapers/16/10/374986844638692/iskusstvo-misaki-kurehito-devushka-paren-vzglyad-nastroenie-ulybka-kniga-cvety-nebo-boku-v-kanojo-ni-furu-yoru.jpg" alt="">
+                   </a>
+                 </div>
+                 <div class="col-md-2">
+                   <a href="#">
+                     <img class="anime" src="https://kartinkin.net/uploads/posts/2021-07/1625503656_41-kartinkin-com-p-anime-pikchi-anime-krasivo-43.jpg" alt="">
+                   </a>
+                 </div>
+                </div>
+              </div>
+              <div class="carousel-item">
+               <div class="row">
+                 <div class="col-md-2">
+                   <a href="#">
+                     <img class="anime" src="https://kartinkin.net/uploads/posts/2021-07/1625873691_48-kartinkin-com-p-syomochnaya-gruppa-anime-anime-krasivo-60.jpg" alt="">
+                   </a>
+                 </div>
+                 <div class="col-md-2">
+                   <a href="#">
+                     <img class="anime" src="https://w-dog.ru/wallpapers/16/10/464978195093068/vocaloid-tagme-xarakter-lo-tyani-zheyi-parker.jpg" alt="">
+                   </a>
+                 </div>
+                 <div class="col-md-2">
+                   <a href="#">
+                     <img class="anime" src="http://pm1.narvii.com/6951/5fb00c2b468133a0e9171e9707d5ec053ff24316r1-1248-1048v2_uhq.jpg" alt="">
+                   </a>
+                 </div>
+                 <div class="col-md-2">
+                  <a href="#">
+                    <img class="anime" src="https://anime-fans.ru/wp-content/uploads/2021/01/Krasivy-anime-arty-tyan-milye-kartinki_16.jpg" alt="">
+                  </a>
+                </div>
+                <div class="col-md-2">
+                  <a href="#">
+                    <img class="anime" src="https://www.zastavki.com/pictures/originals/2015/Anime_Anime_Characters_Cross___Vampire_103415_.jpg" alt="">
+                  </a>
+                </div>
+                <div class="col-md-2">
+                  <a href="#">
+                    <img class="anime" src="https://kartinkin.net/uploads/posts/2021-07/1625756552_1-kartinkin-com-p-anime-personazhi-s-krasnimi-glazami-anime-1.jpg" alt="">
+                  </a>
+                </div>
+               </div>
+             </div>
+             <a class="carousel-control-prev" href="#blogCarousel" role="button" data-slide="prev">
+               <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+               <span class="sr-only">Previous</span>
+             </a>
+             <a class="carousel-control-next" href="#blogCarousel" role="button" data-slide="next">
+               <span class="carousel-control-next-icon" aria-hidden="true"></span>
+               <span class="sr-only">Next</span>
+             </a>
+            </div>
+         </div>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">О проекте</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Заголовок</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Текст</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Картинка</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Приемущества</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Колонки</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Разделитель</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Меню</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Подвал</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Видео</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Форма</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Другое</a>
+      </li>
+    </ul>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
 </html>
-<script type="text/javascript">
-</script>
