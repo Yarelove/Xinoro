6647300f75acee0df3e25165726ebe94157592e3
Egor Udovin <57674283+xoheveras@users.noreply.github.com>
2022-02-10 03:07:48 +0300

    update editor

--- core/views/admin/editor.php
+++ core/views/admin/editor.php
@@ -1,99 +1,14 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <title>Title</title>
+    <title>Редактор страниц</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="vendor/css/xinoro_admin_editor.css">
   </head>
-  <body>
+  <body id="test12">
     <ul class="nav p-2 nav-pills justify-content-center">
-      <li class="nav-item">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Три</a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <div class="carousel slide" data-ride="carousel" id="blogCarousel">
-            <div class="container carousel-inner">
-              <div class="carousel-item active">
-                <div class="row">
-                  <div class="col-md-2">
-                    <a href="#">
-                      <img class="anime" src="https://i.pinimg.com/originals/d6/0e/a3/d60ea3683ce4e8d763e46a829f47f5cd.jpg" alt="">
-                    </a>
-                  </div>
-                  <div class="col-md-2">
-                   <a href="#">
-                     <img class="anime" src="https://proprikol.ru/wp-content/uploads/2020/05/kartinki-glaza-anime-42.jpg" alt="">
-                   </a>
-                 </div>
-                  <div class="col-md-2">
-                   <a href="#">
-                     <img class="anime" src="https://get.wallhere.com/photo/2000x1415-px-anime-girls-schoolgirls-1501001.jpg" alt="">
-                   </a>
-                 </div>
-                  <div class="col-md-2">
-                   <a href="#">
-                     <img class="anime" src="https://phonoteka.org/uploads/posts/2021-05/1621950877_53-phonoteka_org-p-krasivie-anime-arti-krasivo-57.jpg" alt="">
-                   </a>
-                 </div>
-                 <div class="col-md-2">
-                   <a href="#">
-                     <img class="anime" src="https://w-dog.ru/wallpapers/16/10/374986844638692/iskusstvo-misaki-kurehito-devushka-paren-vzglyad-nastroenie-ulybka-kniga-cvety-nebo-boku-v-kanojo-ni-furu-yoru.jpg" alt="">
-                   </a>
-                 </div>
-                 <div class="col-md-2">
-                   <a href="#">
-                     <img class="anime" src="https://kartinkin.net/uploads/posts/2021-07/1625503656_41-kartinkin-com-p-anime-pikchi-anime-krasivo-43.jpg" alt="">
-                   </a>
-                 </div>
-                </div>
-              </div>
-              <div class="carousel-item">
-               <div class="row">
-                 <div class="col-md-2">
-                   <a href="#">
-                     <img class="anime" src="https://kartinkin.net/uploads/posts/2021-07/1625873691_48-kartinkin-com-p-syomochnaya-gruppa-anime-anime-krasivo-60.jpg" alt="">
-                   </a>
-                 </div>
-                 <div class="col-md-2">
-                   <a href="#">
-                     <img class="anime" src="https://w-dog.ru/wallpapers/16/10/464978195093068/vocaloid-tagme-xarakter-lo-tyani-zheyi-parker.jpg" alt="">
-                   </a>
-                 </div>
-                 <div class="col-md-2">
-                   <a href="#">
-                     <img class="anime" src="http://pm1.narvii.com/6951/5fb00c2b468133a0e9171e9707d5ec053ff24316r1-1248-1048v2_uhq.jpg" alt="">
-                   </a>
-                 </div>
-                 <div class="col-md-2">
-                  <a href="#">
-                    <img class="anime" src="https://anime-fans.ru/wp-content/uploads/2021/01/Krasivy-anime-arty-tyan-milye-kartinki_16.jpg" alt="">
-                  </a>
-                </div>
-                <div class="col-md-2">
-                  <a href="#">
-                    <img class="anime" src="https://www.zastavki.com/pictures/originals/2015/Anime_Anime_Characters_Cross___Vampire_103415_.jpg" alt="">
-                  </a>
-                </div>
-                <div class="col-md-2">
-                  <a href="#">
-                    <img class="anime" src="https://kartinkin.net/uploads/posts/2021-07/1625756552_1-kartinkin-com-p-anime-personazhi-s-krasnimi-glazami-anime-1.jpg" alt="">
-                  </a>
-                </div>
-               </div>
-             </div>
-             <a class="carousel-control-prev" href="#blogCarousel" role="button" data-slide="prev">
-               <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-               <span class="sr-only">Previous</span>
-             </a>
-             <a class="carousel-control-next" href="#blogCarousel" role="button" data-slide="next">
-               <span class="carousel-control-next-icon" aria-hidden="true"></span>
-               <span class="sr-only">Next</span>
-             </a>
-            </div>
-         </div>
-        </div>
-      </li>
       <li class="nav-item">
         <a class="nav-link" href="#">О проекте</a>
       </li>
@@ -131,10 +46,23 @@
         <a class="nav-link" href="#">Другое</a>
       </li>
     </ul>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <div class="design-box pt-4 ps-4">
+    	<div onclick="test1();" class="box">
+    		Test 1
+    	</div>
+    	<div onclick="test2();" class="box">
+    		Test 2
+    	</div>
+    </div>
   </body>
+  <script type="text/javascript">
+  	function test1()
+  	{
+  		document.getElementById("test12").innerHTML += ' <div class="intro" id="intro"><div class="intro__body"><div class="intro__logo"><img src="vendor/img/logo/Logo.png" alt=""></div><div class="intro__title">Clear, smart, attractive design<p>for your app, logo, website</p></div><div class="intro__button"><button>READ MORE</button></div><div class="intro__arrow"><div class="arrow-img"><img src="vendor/img/logo/Arrow-down.png" alt=""></div></div></div></div>';
+  	}
+  	function test2()
+  	{
+  		document.getElementById("test12").innerHTML += '<div class="verx"><h1 class="zaglav">Meet Our Leaders</h1></div><div class="seredina"><p>The smartest people work every day to provide the<br>best service and make our clients happy</p></div><content><div class="left"> <!-- левый блок --><div class="onephoto"> <!-- класс первой фотки --><img class="center-pic1" src="vendor/dopfile/1.jpg"> <!-- добовление фотки в левый блок --></div> <div class="txt1-1"> <!-- Класс Первого имени --><h2>Eva Stark</h2>   <!-- имя в первом блоке --></div><div class="txt2-1"> <!-- описание под первой фотографией --><h3>Customer Support</h3></div><div class="txt3-1"><p>Eva is the voice of our brand. She spends hours making our clients feel cared for and enjoying their communication with the company. If you have any suggestions or ideas, you can write to her.</p></div></div><div class="center"> <!-- центральный блок --><div class="twophoto"> <!-- класс второй фотки--><img class="center-pic2" src="vendor/dopfile/3.jpg"> <!-- добавление фотки в центральный блок --></div><div class="txt1-2"> <!-- Класс второго имени --><h2>Julia Bush</h2>  <!-- имя в втором блоке --></div><div class="txt2-2"> <!-- описание под второй фотографией --><h3>Design Director</h3></div><div class="txt3-2"><p>Eva is the voice of our brand. She spends hours making our clients feel cared for and enjoying their communication with the company. If you have any suggestions or ideas, you can write to her.</p></div></div><div class="right"> <!-- правый блок --><div class="threphoto"> <!-- красс третьей фотки --><img class="center-pic3" src="vendor/dopfile/2.jpg"> <!-- добавление фотки в правый блок --></div><div class="txt1-3"> <!-- описание под третьей фотографией --><h2>Max Holden</h2></div><div class="txt2-3"><h3>Founder & Art Director</h3></div><div class="txt3-3"><p>Eva is the voice of our brand. She spends hours making our clients feel cared for and enjoying their communication with the company. If you have any suggestions or ideas, you can write to her.</p></div></div></content>';
+    }
+  </script>
 </html>
