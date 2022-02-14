157ca6d1061bc61fa8c4f20086fcac94e9b25add
Egor Udovin <57674283+xoheveras@users.noreply.github.com>
2022-02-14 13:46:50 +0300

    add library system #15

--- vendor/js/xinoro_admin_panel.js
+++ vendor/js/xinoro_admin_panel.js
@@ -1,3 +1,5 @@
+let selectedPage;
+
 // open madal
 var exampleModal = document.getElementById('exampleModal')
 exampleModal.addEventListener('show.bs.modal', function (event) {
@@ -7,12 +9,46 @@ exampleModal.addEventListener('show.bs.modal', function (event) {
   modalBodyInput.value = recipient
 })
 
+var exampleModal = document.getElementById('exampleModal2')
+exampleModal.addEventListener('show.bs.modal', function (event) {
+  var button = event.relatedTarget
+  var recipient = button.getAttribute('data-bs-whatever')
+  var modalBodyInput = exampleModal.querySelector('.modal-body input')
+  modalBodyInput.value = recipient
+})
+
+
+document.getElementById("savelibbtn").addEventListener("click", function (event) {
+  let libs = "";
+  for(let elem of ["bootstrap","jquery","xohebb"])
+  {
+    if(document.getElementById(elem).checked)
+      libs += elem+";";
+  }
+  window.location.href = "admin/panel?lib&lib_url="+selectedPage+"&libs_select="+libs;
+});
+
 // showinfo
-function showInfo(title, url, controller, action, visible){
+function showInfo(title, url, controller, action, visible, libs){
   document.getElementById("title_info").value = title;
   document.getElementById("url_info").value = url;
   document.getElementById("controller_info").value = controller;
   document.getElementById("action_info").value = action;
+  let library = libs.split(";");
+
+  selectedPage = url;
+
+  for (let elem of  document.querySelectorAll('.custom-checkbox')) {
+    elem.checked = false;
+  }
+
+  library.forEach(element => {
+    try
+    {
+      document.getElementById(element).checked = true;
+    }
+    catch{}
+  });
 
   if(visible > 0)
   {
