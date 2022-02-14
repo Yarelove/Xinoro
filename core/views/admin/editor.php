84578bfe2c7db83b501a543f0cf3b30ea00d94ab
Egor Udovin <57674283+xoheveras@users.noreply.github.com>
2022-02-14 17:58:58 +0300

    add first block in editor

--- core/views/admin/editor.php
+++ core/views/admin/editor.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html lang="ru">
   <head>
     <title>Редактор страниц</title>
     <!-- Required meta tags -->
@@ -58,8 +58,14 @@
   <script type="text/javascript">
   	function test1()
   	{
-  		document.getElementById("test12").innerHTML += ' <div class="intro" id="intro"><div class="intro__body"><div class="intro__logo"><img src="vendor/img/logo/Logo.png" alt=""></div><div id="test-edit-text" class="intro__title">Clear, smart, attractive design<p>for your app, logo, website</p></div><div class="intro__button"><button>READ MORE</button></div><div class="intro__arrow"><div class="arrow-img"><img src="vendor/img/logo/Arrow-down.png" alt=""></div></div></div></div>';
-      document.getElementById("test-edit-text").contentEditable = true;
+  		document.getElementById("test12").innerHTML += '<div id="content_first"><div id="introduction"><div class="inner first_intro" id="test-edit-text"><p>OUR COMPANY</p></div><div class="inner second_intro" id="test-edit-text"><p>Bring Your Ideas to Life</p></div><div class="inner third_intro" id="test-edit-text"><p>Everything that you dreamed of can be brought to life exactly at the </p><p>moment when you decide to win</p></div><div id="arrow"><img src="vendor/image/header-fedya-1/arrow.png"></div></div></div>';
+      
+      // Включаем возможность редактирования текста
+      for(let div of document.querySelectorAll("#xinoro_text_ed"))
+      {
+        div.contentEditable = true;
+      }
+
     }
   	function test2()
   	{
